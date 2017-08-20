cf56f8247880c426f3a1b283d63d3d03fc48ff7f
Anis Marrouchi <anismarrouchi@hotmail.com>
2017-08-20 15:14:22 +0100

    Add settings field to the user model

---
+++ app/Models/User.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $table = 'users';
+
+    protected $settingsData;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'settings'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * Settings getter
+     *
+     * @return mixed
+     */
+    public function getSettingsAttribute()
+    {
+        if ($this->settingsData == null) {
+            $this->settingsData = json_decode($this->attributes['settings'], true);
+        }
+
+        return $this->settingsData;
+    }
+
+    /**
+     * Settings setter
+     *
+     * @param array $settings
+     */
+    public function setSettingsAttribute(array $settings)
+    {
+        $this->settingsData = null;
+        $this->attributes['settings'] = json_encode($settings);
+    }
+}
